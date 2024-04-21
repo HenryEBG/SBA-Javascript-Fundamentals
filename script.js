bf182e2ecc731a206539f7c91cf7f701502cab51
HenryEBG <berganza386@gmail.com>
2024-04-20 21:41:45 -0400

    creating pseudocode

---
+++ script.js
@@ -0,0 +1,116 @@
+// The provided course information.
+const CourseInfo = {
+  id: 451,
+  name: "Introduction to JavaScript"
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
+    }
+  ]
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
+  }
+];
+
+function getLearnerData(course, ag, submissions) {
+  // here, we would process this data to achieve the desired result.
+
+  // buscar los diferentes id de estudiantes en submissions
+  
+  // recorrer para cada estudiante el proceso
+
+  // agregar el codigo del estudiante al objeto como id.
+  // por cada estudiante procesar todas las asignaciones que ya se hayan vencido
+  // si la tarea esta vencida restarle a su nota el 10% del valor 
+  // sumar la nota a un total
+  // sumar la nota maxima a otro total
+  // calcular el porcentaje de la materia dividiendo la nota recibida(con el descuento) entre el total
+  // asignar el resultado como un atributo del objeto donde la llave es el assigment_id
+  // la suma de las notas totales / entre la nota maxima total y guardarla como el atributo avg
+  // empujar el objeto al array result.
+
+  const result = [
+    {
+      id: 125,
+      avg: 0.985, // (47 + 150) / (50 + 150)
+      1: 0.94, // 47 / 50
+      2: 1.0 // 150 / 150
+    },
+    {
+      id: 132,
+      avg: 0.82, // (39 + 125) / (50 + 150)
+      1: 0.78, // 39 / 50
+      2: 0.833 // late: (140 - 15) / 150
+    }
+  ];
+
+  return result;
+}
+
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
+console.log(result);
